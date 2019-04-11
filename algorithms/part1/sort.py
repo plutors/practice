2d0e26a6ebe1796e331099ed2fcf864deda6b68f
Plutors <plutors@163.com>
2019-04-11 19:53:24 +0800

    add select sort

--- algorithms/part1/sort.py
+++ algorithms/part1/sort.py
@@ -11,5 +11,26 @@ def insert_sort(list_=None):
     return list_
 
 
+def select_sort(list_=None):
+    if list_ is None:
+        list_ = []
+    for i in range(0, len(list_)-1):
+        key = list_[i]
+        index = i + 1
+        for j in range(i+1, len(list_)):
+            if list_[j] < key:
+                key = list_[j]
+                index = j
+        if index != i+1:
+            list_[index] = list_[i]
+            list_[i] = key
+    return list_
+
+
+def merge_sort(list_=None):
+    return list_
+
+
 a = [4, 3, 0, 9, 7, 5]
-print(insert_sort(a))
+#print(insert_sort(a))
+print(select_sort(a))
